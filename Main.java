fb3c6932d53d15c4d7f0f4469313cdb50f86facd
14Kunalsharma <166681796+14Kunalsharma@users.noreply.github.com>
2025-09-06 21:06:20 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+       Scanner sc=new Scanner(System.in);
+		System.out.print("Enter no. of rows: ");
+		int x=sc.nextInt();
+		for (int i=1; i<=x; i++){
+			for (int j=1; j<=i; j++){
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+    }
+}
